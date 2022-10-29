4136067c230d230902b9ee57f83e6a355bccfefe
segsmachine <sheryceeastland@gmail.com>
2022-10-28 23:32:43 -0700

    Update edgentweaks.user.js

--- edgentweaks.user.js
+++ edgentweaks.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Edgentweaks
 // @namespace    https://github.com/segsmachine/Edgenuity-Scripts/
-// @version      1.6.1
+// @version      1.6.2
 // @author       GW, segsmachine
 // @description  Adds tweaks to edgenuity
 // @source       https://github.com/segsmachine/Edgenuity-Scripts/blob/main/edgentweaks.user.js
+// @downloadURL  https://github.com/segsmachine/Edgenuity-Scripts/raw/main/edgentweaks.user.js
 // @updateURL    https://github.com/segsmachine/Edgenuity-Scripts/raw/main/edgentweaks.user.js
 // @supportURL   https://github.com/segsmachine/Edgenuity-Scripts/issues
 // @match        *://*.core.learn.edgenuity.com/*
@@ -204,8 +205,6 @@ window.configbutton.style.color = "#f9a619" */
                         /* Keyboard Hotkeys */
 
                         shortcut('Ctrl+Shift+H', function (event, handler) {
-                            /* Prevent the default refresh event under WINDOWS system
-                            event.preventDefault(); */
                                 $("#HideButton")
                                         .click();
                                 $("#userconsole")
@@ -213,8 +212,6 @@ window.configbutton.style.color = "#f9a619" */
                                                 .is(":checked"));
                         })
                         shortcut('Ctrl+Shift+G', function (event, handler) {
-                            /* Prevent the default refresh event under WINDOWS system
-                            event.preventDefault(); */
                                 $("#GuessPractice")
                                         .click();
                                 $("#userconsole")
@@ -222,8 +219,6 @@ window.configbutton.style.color = "#f9a619" */
                                                 .is(":checked"));
                         })
                         shortcut('Ctrl+Shift+A', function () {
-                            /* Prevent the default refresh event under WINDOWS system
-                            event.preventDefault(); */
                                 $("#AutoAdvance")
                                         .click();
                                 $("#userconsole")
@@ -231,8 +226,6 @@ window.configbutton.style.color = "#f9a619" */
                                                 .is(":checked"));
                         })
                         shortcut('Ctrl+Shift+S', function () {
-                            /* Prevent the default refresh event under WINDOWS system
-                            event.preventDefault(); */
                                 $("#SearchButton > button")
                                         .click();
                                 $("#userconsole")
@@ -240,17 +233,13 @@ window.configbutton.style.color = "#f9a619" */
                         })
 
                         shortcut('Ctrl+Enter', function () {
-                            /* Prevent the default refresh event under WINDOWS system
-                            event.preventDefault(); */
                                 $("#stageFrame")
                                         .contents().find(".uibtn.uibtn-blue.uibtn-arrow-next")[0].click();
                                 $("#userconsole")
                                         .prepend("<li>Next hotkey pressed ");
                         })
 
-                    /* shortcut("Ctrl+Enter", function (event, handler) {
-                            //Prevent the default refresh event under WINDOWS system
-                            event.preventDefault();
+                    /* shortcut("Ctrl+Enter", function () {
                                 $("#stageFrame")
                                         .contents().find("#btnCheck")[0].click();
                                 $("#userconsole")
